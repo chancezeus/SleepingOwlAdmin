7430ab6a2b7e6666d72fb5785cb7416763ce8605
Mark van Beek <flipvb@gmail.com>
2017-03-21 17:45:26 +0100

    Update

--- src/Display/Column/Image.php
+++ src/Display/Column/Image.php
@@ -2,12 +2,19 @@
 
 namespace SleepingOwl\Admin\Display\Column;
 
+use Storage;
+
 class Image extends NamedColumn
 {
     /**
      * @var string
      */
     protected $imageWidth = '80px';
+
+    /**
+     * @var string
+     */
+    protected $disk;
 
     /**
      * @var string
@@ -35,13 +42,46 @@ class Image extends NamedColumn
     }
 
     /**
+     * @return string
+     */
+    public function getDisk()
+    {
+        if (!$this->disk) {
+            return $this->defaultDisk();
+        }
+
+        return $this->disk;
+    }
+
+    /**
+     * @param string $disk
+     *
+     * @return $this
+     */
+    public function setUploadDisk($disk)
+    {
+        $this->disk = $disk;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function defaultDisk()
+    {
+        return config('sleeping_owl.filesUploadDisk', 'public');
+    }
+
+    /**
      * @return array
      */
     public function toArray()
     {
         $value = $this->getModelValue();
+
         if (! empty($value) && (strpos($value, '://') === false)) {
-            $value = asset($value);
+            $value = is_string($value) ? Storage::disk($this->getDisk())->url($value) : null;
         }
 
         return parent::toArray() + [
